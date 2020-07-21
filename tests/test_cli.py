6c4342d81ae81bcc6f0d1aa9cb5237297153d52d
Harsh Parekh <h.x.dev@outlook.com>
2020-07-21 08:52:29 -0400

    Refactor tests/test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,25 +11,27 @@ from tasks3 import tasks3
 from tasks3 import cli
 
 
-def test_command_line_interface_help():
+@pytest.fixture
+def cli_runner():
+    return CliRunner()
+
+
+def test_command_line_interface_main(cli_runner):
     """Test the CLI"""
-    runner = CliRunner()
-    main_result = runner.invoke(cli.main)
+    main_result = cli_runner.invoke(cli.main)
     assert main_result.exit_code == 0
     assert package_tasks3.__doc__ in main_result.output
 
 
-def test_command_line_interface_version():
+def test_command_line_interface_version(cli_runner):
     """Test the CLI (--version)"""
-    runner = CliRunner()
-    version_result = runner.invoke(cli.main, ["--version"])
+    version_result = cli_runner.invoke(cli.main, ["--version"])
     assert version_result.exit_code == 0
     assert package_tasks3.__version__ in version_result.output
 
 
-def test_command_line_interface_help():
+def test_command_line_interface_help(cli_runner):
     """Test the CLI (--help)"""
-    runner = CliRunner()
-    help_result = runner.invoke(cli.main, ["--help"])
+    help_result = cli_runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
     assert "--help     Show this message and exit." in help_result.output
