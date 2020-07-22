963dbf0b8b75fd8de761b1ecda356b6b58b12544
Harsh Parekh <h.x.dev@outlook.com>
2020-07-22 13:34:37 -0400

    Remove period(.) from docstring

--- tasks3/__init__.py
+++ tasks3/__init__.py
@@ -1,4 +1,4 @@
-"""tasks3 is a commandline tool to create and manage tasks and todo lists."""
+"""tasks3 is a commandline tool to create and manage tasks and todo lists"""
 
 __author__ = """Harsh Parekh"""
 __email__ = "harsh_parekh@outlook.com"
