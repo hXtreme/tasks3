ea3df083397f74416b93a76edc91881fef6b6122
Harsh Parekh <harsh_parekh@outlook.com>
2022-04-30 22:54:53 -0400

    Use full import paths

--- tasks3/db/__init__.py
+++ tasks3/db/__init__.py
@@ -1,9 +1,9 @@
 """Database for tasks3"""
 
-from .db import init, drop, purge  # noqa: F401
-from .models import Task  # noqa: F401
+from tasks3.db.db import init, drop, purge  # noqa: F401
+from tasks3.db.models import Task  # noqa: F401
 
-from .extension import session_scope  # noqa: F401
+from tasks3.db.extension import session_scope  # noqa: F401
 
 __all__ = [
     "init",
