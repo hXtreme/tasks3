200043e44ba968174660f33f81067e091155ce0b
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-01 18:35:35 -0400

    Add overrides for tasks3.add method
    - Override 1: Specify task fields and db_engine.
    - Override 2: Pass a Task object and db_engine.
    - Update tests.

--- tests/test_tasks3_tasks3.py
+++ tests/test_tasks3_tasks3.py
@@ -54,7 +54,37 @@ def get_db_engine(tmp_path: Path, backend: str) -> str:
     return engine
 
 
-def test_task_add(
+def test_task_add1(
+    tmp_path: Path,
+    title,
+    urgency,
+    importance,
+    tags,
+    anchor_path,
+    description,
+    db_backend: str,
+):
+    db_engine = get_db_engine(tmp_path, db_backend)
+    task = db.Task(
+        title=title,
+        urgency=urgency,
+        importance=importance,
+        tags=tags,
+        folder=anchor_path,
+        description=description,
+    )
+    id = tasks3.add(task, db_engine)
+    with db.session_scope(db_engine) as session:
+        task: db.Task = session.query(db.Task).filter_by(id=id).one()
+        assert task.title == title
+        assert task.urgency == urgency
+        assert task.importance == importance
+        assert task.tags == tags
+        assert task.folder == anchor_path
+        assert task.description == description
+
+
+def test_task_add2(
     tmp_path: Path,
     title,
     urgency,
@@ -66,7 +96,7 @@ def test_task_add(
 ):
     db_engine = get_db_engine(tmp_path, db_backend)
     id = tasks3.add(
-        title=title,
+        title,
         urgency=urgency,
         importance=importance,
         tags=tags,
@@ -84,7 +114,46 @@ def test_task_add(
         assert task.description == description
 
 
-def test_task_edit(tmp_path: Path, db_backend: str):
+def test_task_edit1(tmp_path: Path, db_backend: str):
+    db_engine = get_db_engine(tmp_path, db_backend)
+    title, urgency, importance, tags, anchor_path, description = (
+        "New Title",
+        4,
+        4,
+        ["new-tags"],
+        "/tmp",
+        "New description.",
+    )
+    task = db.Task(
+        title="Old title",
+        urgency=2,
+        importance=2,
+        tags=["old-tags"],
+        folder=".",
+        description="Old description",
+    )
+    id = tasks3.add(task, db_engine)
+    tasks3.edit(
+        id=id,
+        db_engine=db_engine,
+        title=title,
+        urgency=urgency,
+        importance=importance,
+        tags=tags,
+        anchor_folder=anchor_path,
+        description=description,
+    )
+    with db.session_scope(db_engine) as session:
+        task: db.Task = session.query(db.Task).filter_by(id=id).one()
+        assert task.title == title
+        assert task.urgency == urgency
+        assert task.importance == importance
+        assert task.tags == tags
+        assert task.folder == anchor_path
+        assert task.description == description
+
+
+def test_task_edit2(tmp_path: Path, db_backend: str):
     db_engine = get_db_engine(tmp_path, db_backend)
     title, urgency, importance, tags, anchor_path, description = (
         "New Title",
@@ -95,7 +164,7 @@ def test_task_edit(tmp_path: Path, db_backend: str):
         "New description.",
     )
     id = tasks3.add(
-        title="Old title",
+        "Old title",
         urgency=2,
         importance=2,
         tags=["old-tags"],
@@ -123,7 +192,42 @@ def test_task_edit(tmp_path: Path, db_backend: str):
         assert task.description == description
 
 
-def test_task_remove(
+def test_task_remove1(
+    tmp_path: Path,
+    title,
+    urgency,
+    importance,
+    tags,
+    anchor_path,
+    description,
+    db_backend: str,
+):
+    db_engine = get_db_engine(tmp_path, db_backend)
+    temp_task = db.Task(
+        title=title,
+        urgency=urgency,
+        importance=importance,
+        tags=tags,
+        folder=anchor_path,
+        description=description,
+    )
+    id = tasks3.add(temp_task, db_engine)
+    with db.session_scope(db_engine) as session:
+        task: db.Task = session.query(db.Task).filter_by(id=id).one()
+        assert task is not None
+    task = tasks3.remove(id, db_engine)
+    assert task.title == title
+    assert task.urgency == urgency
+    assert task.importance == importance
+    assert task.tags == tags
+    assert task.folder == anchor_path
+    assert task.description == description
+    with db.session_scope(db_engine) as session:
+        task: db.Task = session.query(db.Task).filter_by(id=id).first()
+        assert task is None
+
+
+def test_task_remove2(
     tmp_path: Path,
     title,
     urgency,
@@ -135,7 +239,7 @@ def test_task_remove(
 ):
     db_engine = get_db_engine(tmp_path, db_backend)
     id = tasks3.add(
-        title=title,
+        title,
         urgency=urgency,
         importance=importance,
         tags=tags,
