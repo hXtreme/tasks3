f16d281b415d4bd746c399d61c05b28e3e5354cb
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-02 19:39:53 -0400

    Bump version: 0.2.8 → 0.3.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/hXtreme/tasks3",
-    version="0.2.8",
+    version="0.3.0",
     zip_safe=False,
 )
