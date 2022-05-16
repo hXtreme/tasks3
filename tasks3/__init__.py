c8d7e43134cb0744662283a0b02dfae86db9c04b
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-16 17:38:49 -0400

    Bump version: 0.5.1 → 0.5.2

--- tasks3/__init__.py
+++ tasks3/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Harsh Parekh"""
 __email__ = "harsh_parekh@outlook.com"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from tasks3.tasks3 import add, edit, remove, search  # noqa: F401
